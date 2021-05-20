68ed359b5b2d966b5d131c2e12835cc50253565e
Alvin Novian <alvinnovian883@gmail.com>
2021-05-20 10:15:50 +0700

    Version 0.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 export { default as Modal } from './ui/modal/modal';
 export { NewModal } from './ui/modal/modal';
 export { default as useModal } from './hooks/use-modal';
+export { default as useTimer } from './hooks/use-timer';
 export * from './types';
