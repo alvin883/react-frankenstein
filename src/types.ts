cc69ea4d41c69fe40e18e110adc486cf85358f28
Alvin Novian <alvinnovian883@gmail.com>
2021-05-25 18:33:22 +0700

    feat: init button and style-combiner

--- src/types.ts
+++ src/types.ts
@@ -34,17 +34,21 @@ export type StandardComponentTypeProps<TagType, Props> = Props &
  * Standardize component type combined with dynamic as props and default tag
  * type
  */
-export type StandardComponentType<DefaultTag extends React.ElementType, Props> =
-  (<TagType extends React.ElementType = DefaultTag>(
-    props: StandardComponentTypeProps<TagType, Props>,
-  ) => React.ReactElement) & {
-    propTypes: React.WeakValidationMap<Props>;
-  };
+export type StandardComponentType<
+  DefaultTag extends React.ElementType,
+  Props
+> = (<TagType extends React.ElementType = DefaultTag>(
+  props: StandardComponentTypeProps<TagType, Props>,
+) => React.ReactElement) & {
+  propTypes?: React.WeakValidationMap<Props>;
+};
 
-export type StandardComponentWithClassName<DT extends React.ElementType, P> =
-  StandardComponentType<DT, P> & {
-    styles: string;
-  };
+export type StandardComponentWithClassName<
+  DT extends React.ElementType,
+  P
+> = StandardComponentType<DT, P> & {
+  styles: string;
+};
 
 export type ClassNamesType<S extends string> = {
   [key in S]?: string;
--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -1,22 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './button.module.scss';
 import { StandardComponentType } from '../../types';
+import { stylesCombinerFn } from '../../utils/styles-combiner';
 
-let DEFAULT_TAG: 'div' = 'div';
+let DEFAULT_TAG: 'button' = 'button';
+
+type SizeEnum = 'small' | 'normal' | 'jumbo';
+type VariantEnum = 'fab' | 'fab-circle' | 'outlined' | 'contained';
+type ColorEnum = 'primary' | 'secondary';
 
 export type ButtonProps = {
-  size?: 'small' | 'normal' | 'jumbo';
+  classNames?: object;
+  color?: ColorEnum;
+  disabled?: boolean;
+  iconEnd?: React.ElementType;
+  iconStart?: React.ElementType;
+  isLoading?: boolean;
+  size?: SizeEnum;
+  text?: string;
+  variant?: VariantEnum;
 };
 
 const Button: StandardComponentType<typeof DEFAULT_TAG, ButtonProps> = (
   props,
 ) => {
-  return <button className={props.size}></button>;
+  // Default props
+  const {
+    as: Tag = DEFAULT_TAG,
+    classNames = {},
+    className,
+    size = 'normal',
+    variant = 'contained',
+    color = 'primary',
+    disabled = false,
+    isLoading = false,
+  } = props;
+
+  const c = stylesCombinerFn(styles, classNames);
+
+  return (
+    <Tag
+      className={c('button', className, {
+        [c[`size-${size}`]]: size,
+        [c[`color-${color}`]]: color,
+        [c[`variant-${variant}`]]: variant,
+        [c[`is-loading`]]: isLoading,
+        [c[`is-disabled`]]: disabled,
+      })}
+      disabled={props.disabled || isLoading}
+    ></Tag>
+  );
 };
 
 Button.propTypes = {
-  size: PropTypes.string,
+  size: PropTypes.oneOf<SizeEnum>(['small', 'normal', 'jumbo']),
+  variant: PropTypes.oneOf<VariantEnum>(['fab', 'fab-circle']),
+  color: PropTypes.oneOf<ColorEnum>(['primary', 'secondary']),
 };
 
 export default Button;
