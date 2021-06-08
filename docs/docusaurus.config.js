eb28b2a6cbaac05892f7df9e246fe7f3cb703414
Alvin Novian <alvinnovian883@gmail.com>
2021-06-08 11:39:07 +0700

    doc: change repo information & url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,8 +7,8 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'alvin883', // Usually your GitHub org/user name.
+  projectName: 'react-frankenstein', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'React Frankenstein',
@@ -25,7 +25,7 @@ module.exports = {
         },
         // { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/alvin883/react-frankenstein',
           label: 'GitHub',
           position: 'right',
         },
@@ -48,15 +48,7 @@ module.exports = {
           items: [
             {
               label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://stackoverflow.com/questions/tagged/react-frankenstein',
             },
           ],
         },
@@ -69,7 +61,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/alvin883/react-frankenstein',
             },
           ],
         },
@@ -85,13 +77,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/alvin883/react-frankenstein/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/alvin883/react-frankenstein/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
